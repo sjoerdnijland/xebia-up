c17af30dc9fed2352e590edacae5b6d5100526cf
sjoerdnijland <sjoerd.nijland@outlook.com>
2026-06-17 08:26:24 +0200

    monolog

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
